c7f8f8cf70fa1007ced69405c7431d4f29c65423
Yvette <yvettecook@gmail.com>
2014-12-21 18:15:48 +0000

    Added JSHint to app and grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,23 @@
 module.exports = function(grunt){
 
   grunt.initConfig({
+
     jasmine_node: {
       options: {
         forceExit: true,
       },
-
       all: ['spec/']
+    },
+
+    jshint: {
+      files: ['Gruntfile.js']
     }
+
   });
 
   grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('default', ['jasmine_node']);
+  grunt.registerTask('default', ['jasmine_node', 'jshint']);
 
 };
